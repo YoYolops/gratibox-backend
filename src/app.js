408d92b214a5a6e5aa389bfda507e841d60b9b1c
yohan <yohan@yo.yo>
2023-01-30 17:35:19 -0300

    Preparing deploy

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
+import path from "node:path";
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use( cors() )
 app.use( express.json() )
 app.use( routes );
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "build", "index.html")))
 
 export default app;
